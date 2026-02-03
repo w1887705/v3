8c0fe58aa143314c1a331b55440917efbed789e8
Aran <w1887705@westminster.ac.uk>
2026-02-03 21:40:54 +0000

    seed script

--- build.sh
+++ build.sh
@@ -9,3 +9,7 @@ python manage.py migrate --noinput
 
 # Optional: seed the demo user (safe to run multiple times if the command handles duplicates)
 python manage.py seed_demo
+
+echo "Running Seed Script..."
+python manage.py seed_demo --traceback
+echo "Seed Script Finished."
