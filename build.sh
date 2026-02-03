2f5ce394ddf57ac7119f0a8bfee62c4f454a5c77
Aran <w1887705@westminster.ac.uk>
2026-02-03 22:35:15 +0000

    render

--- build.sh
+++ build.sh
@@ -5,11 +5,3 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-
-# Optional: seed the demo user (safe to run multiple times if the command handles duplicates)
-python manage.py seed_demo
-
-echo "Running Seed Script..."
-python manage.py seed_demo --traceback
-echo "Seed Script Finished."
