22ef5c6518146e7f9445e15a6938ef4f684d42e3
Aran <w1887705@westminster.ac.uk>
2026-02-03 22:58:51 +0000

    okay

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-# Collect static (safe to run on start as well)
+echo "Collecting static..."
 python manage.py collectstatic --noinput
 
-# Run migrations
+echo "Running migrations..."
 python manage.py migrate --noinput
 
-# Start app
+echo "Seeding demo data..."
+python manage.py seed_demo
+
+echo "Starting app..."
 exec gunicorn task10_project.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers ${WEB_CONCURRENCY:-2} --timeout 120
